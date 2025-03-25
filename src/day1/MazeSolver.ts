3a2e4c015bc6385f624bceb5fd2e6b1b5ae81b82
Andrii Lavreniuk <andrii.lavreniuk@hpe.com>
2025-03-25 18:41:42 +0100

    updated setup

---
+++ src/day1/MazeSolver.ts
@@ -0,0 +1,9 @@
+function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {
+    //1. Base Case
+    // off the map
+    
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+
+}
